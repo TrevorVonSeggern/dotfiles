3bfc9b3a526e63f8c32ed9512f9f0ac81e541620
Trevor Von Seggern <tvoman2@gmail.com>
2020-06-12 17:51:40 -0600

    checks for the install script

--- install.sh
+++ install.sh
@@ -6,20 +6,7 @@ source .zshenv
 
 # zsh
 [[ -f ~/.zshenv ]] && rm ~/.zshenv
-ln -s $CDIR/.zshenv ~/.zshenv
+[[ ! -d ~/.conf ]] && ln -s $CDIR/.zshenv ~/.zshenv
 
 [[ ! -f ~/.config ]] && ln -s $CDIR ~/.config
 
-mkdir -p $HOME/.cache/vim/{undo,swap,backup} $HOME/.config
-
-if empty("$XDG_CACHE_HOME")
-    let $XDG_CACHE_HOME="$HOME/.cache"
-endif
-
-if empty("$XDG_CONFIG_HOME")
-    let $XDG_CONFIG_HOME="$HOME/.config"
-endif
-
-if empty("$XDG_DATA_HOME")
-    let $XDG_DATA_HOME="$HOME/.local/share"
-endif
