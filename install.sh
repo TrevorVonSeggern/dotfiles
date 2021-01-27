a3a5333976f81585e375cc183221228412ded7a3
Trevor Von Seggern <tvoman2@gmail.com>
2021-01-27 12:02:15 -0700

    zsh updates

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 CDIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+
 # zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 [[ -f $HOME/.zshenv ]] && rm $HOME/.zshenv
 
 ln -s "$CDIR/.zshenv" $HOME/.zshenv
--- zsh/downloadzsh.sh
+++ zsh/downloadzsh.sh
@@ -2,7 +2,7 @@
 
 sudo apt install zsh
 
-# install zsh
+# install oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
