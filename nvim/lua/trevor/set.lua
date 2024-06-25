0640897197dcd581397b7468d4a76d312f1382ea
Trevor Von Seggern <tvonseggern@ftpins.com>
2024-06-25 11:53:33 -0600

    nvim leader copy goes to system clip

--- nvim/lua/trevor/set.lua
+++ nvim/lua/trevor/set.lua
@@ -16,6 +16,15 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+-- Yank into system clipboard
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y') -- yank motion
+vim.keymap.set({'n', 'v'}, '<leader>Y', '"+Y') -- yank line
+-- Delete into system clipboard
+vim.keymap.set({'n', 'v'}, '<leader>d', '"+d') -- delete motion
+vim.keymap.set({'n', 'v'}, '<leader>D', '"+D') -- delete line
+-- Paste from system clipboard
+vim.keymap.set('n', '<leader>p', '"+p')  -- paste after cursor
+vim.keymap.set('n', '<leader>P', '"+P')  -- paste before cursor
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
