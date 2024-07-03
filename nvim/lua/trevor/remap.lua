332ae9fd86548e99edf797ccda81f0abcfc33edc
Trevor Von Seggern <tvonseggern@ftpins.com>
2024-07-03 12:29:41 -0600

    nvim yyp keeps cursor position

--- nvim/lua/trevor/remap.lua
+++ nvim/lua/trevor/remap.lua
@@ -8,6 +8,8 @@ vim.keymap.set("n", "<C-j>", "<cmd>m +1<CR>")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+vim.keymap.set("n", "yyp", "<cmd>co.<CR>")
+
 vim.keymap.set("n", "<leader>r", ":IncRename ")
 --vim.keymap.set("n", "<leader>r", function()
   --return ":IncRename " .. vim.fn.expand("<cword>")
